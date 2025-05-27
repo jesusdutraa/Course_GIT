ae16303c1a9dfa8ea2f28ea56cb3fbda067714c6
jesusdutraa <gabrieljesusdev@gmail.com>
2025-05-27 18:23:30 -0300

    second commit

---
+++ course_git.java
@@ -0,0 +1,14 @@
+public class course_git {
+  public static void main(String[] args) {
+
+    String nome = "curso_git";
+    System.out.println(nome);
+    int n = 2;
+    System.out.println(n);
+  
+}
+}
+
+
+
+
