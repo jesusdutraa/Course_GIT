d0440537f3f99a0940493b76a40d8c333875f866
jesusdutraa <gabrieljesusdev@gmail.com>
2025-05-28 20:59:07 -0300

    novas mudanças

--- course_git.java
+++ course_git.java
@@ -4,7 +4,9 @@ public class course_git {
     String nome = "curso_git";
     System.out.println(nome);
     int n = 2;
-    System.out.println(n);
+       System.out.println(n);
+    int n3 = 10;
+    System.out.println(n3);
 
     
   
